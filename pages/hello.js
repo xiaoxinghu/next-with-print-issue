e72e4994c4160b0a1a34f5d9292c6bc0391bc937
Xiaoxing Hu <xiaoxing@huxx.org>
2021-10-03 14:22:53 +1300

    init

---
+++ pages/hello.js
@@ -0,0 +1,11 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx, Heading } from 'theme-ui'
+
+export default function Hello() {
+  return (
+    <div sx={{ py: 3 }}>
+      <Heading>Hello</Heading>
+    </div>
+  )
+}
